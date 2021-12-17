c07a27492aa29d347d8ca4b2a57b14b363c424df
Guy Arbitman <guy20495@gmail.com>
2021-12-17 08:31:08 +0200

    Added scheduled trigger to update btfhub

--- devops/deploy.sh
+++ devops/deploy.sh
@@ -5,3 +5,9 @@ IMAGE_NAME=${IMAGE_NAME:-"us.gcr.io/seekret/btfhub"}
 gcloud run deploy btfhub --image "$IMAGE_NAME" --region us-east1 --allow-unauthenticated --min-instances=1 \
   --max-instances=5 --service-account=$SERVICE_ACCOUNT --format=json --project $PROJECT_ID \
   --set-env-vars="ARCHIVE_DIR=/archive,TOOLS_DIR=/app/tools" --memory 2Gi
+
+gcloud scheduler jobs create http update-btfhub --schedule "0 12 1 * *" \
+   --http-method=POST \
+   --uri=https://btfhub.seekret.com \
+   --oidc-service-account-email=$SERVICE_ACCOUNT   \
+   --oidc-token-audience=https://btfhub.seekret.com
