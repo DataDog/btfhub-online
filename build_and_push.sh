2626ca8c2d20de0e96d88e0dbddfbff683c3f390
Guy Arbitman <guy20495@gmail.com>
2022-02-06 18:35:02 +0200

    removed seekret from project

--- build_and_push.sh
+++ build_and_push.sh
@@ -2,15 +2,20 @@
 
 CURRENT_DIR=$(dirname $(realpath "$0"))
 ROOT_DIR=$(dirname "$CURRENT_DIR")
-IMAGE_NAME=${IMAGE_NAME:-"us.gcr.io/seekret/btfhub"}
 DOCKER_COMMAND=${DOCKER_COMMAND:-"docker build"}
 
 if [[ -z $1 ]]; then
-  echo "usage: $(basename $0) <version>"
+  echo "usage: $(basename $0) <version> <docker image>"
+  exit 1
+fi
+
+if [[ -z $2 ]]; then
+  echo "usage: $(basename $0) <version> <docker image>"
   exit 1
 fi
 
 VERSION=$1
+IMAGE_NAME=$2
 
 set -e
 echo "Building docker image ${IMAGE_NAME}:${VERSION}"
