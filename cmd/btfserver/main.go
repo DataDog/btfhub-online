08aefcb70abeb7ec3f71a6df33389a358c6c03a6
Guy Arbitman <guy20495@gmail.com>
2022-02-06 08:23:37 +0200

    Added optional push gateway of prometheus (#9)
    * Added optional push gateway of prometheus
    
    * Fixed typo

--- cmd/btfserver/main.go
+++ cmd/btfserver/main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
+	"sort"
 	"time"
 
 	"github.com/alexflint/go-arg"
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"github.com/heptiolabs/healthcheck"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/prometheus/client_golang/prometheus/push"
 	metrics "github.com/slok/go-http-metrics/metrics/prometheus"
 	"github.com/slok/go-http-metrics/middleware"
 	ginmiddleware "github.com/slok/go-http-metrics/middleware/gin"
@@ -20,31 +24,39 @@ import (
 
 // args is the arguments to the server. Each of the arguments can be supplied via environment variable or command line.
 var args struct {
-	BucketName  string `arg:"-b,env:BUCKET_NAME,required"`
-	ToolsDir    string `arg:"-t,env:TOOLS_DIR,required"`
-	Port        string `arg:"-p,env:PORT" default:"8080"`
-	MetricsPort string `arg:"-m,env:METRICS_PORT"`
+	BucketName            string `arg:"-b,env:BUCKET_NAME,required"`
+	ToolsDir              string `arg:"-t,env:TOOLS_DIR,required"`
+	Port                  string `arg:"-p,env:PORT" default:"8080"`
+	DisableMonitoring     bool   `arg:"--no-monitoring,env:NO_MONITORING" default:"false"`
+	PrometheusPushGateway string `arg:"--push-gateway,env:PUSH_GATEWAY"`
 }
 
-func main() {
-	arg.MustParse(&args)
+const (
+	prometheusPushGatewayJobName = "btfhub-online-push"
+)
 
-	toolsDir, err := path.GetAbsolutePath(args.ToolsDir)
-	if err != nil {
-		log.Fatalf("Failed to find %s due to: %+v", args.ToolsDir, err)
+func pushPrometheusMetrics(gateway string, registry *prometheus.Registry) {
+	for {
+		if err := push.New(gateway, prometheusPushGatewayJobName).Gatherer(registry).Push(); err != nil {
+			log.Printf("ERROR: failed pushing prometheus metrics due to: %+v", err)
+		}
+		time.Sleep(time.Minute)
 	}
+}
 
-	archive, err := btfarchive.NewGCSBucketArchive(args.BucketName)
-	if err != nil {
-		log.Fatalf("Failed initialize GCP bucket %q archive due to: %+v", args.BucketName, err)
-	}
-
-	routesHandler := handlers.NewRoutesHandler(archive, toolsDir)
-
-	// Create our middleware.
-	prometheusMiddleware := middleware.New(middleware.Config{
-		Recorder: metrics.NewRecorder(metrics.Config{}),
+func printAllRoutes(engine *gin.Engine) {
+	fmt.Println("Routes: ")
+	routes := engine.Routes()
+	sort.Slice(routes, func(i, j int) bool {
+		return routes[i].Path < routes[j].Path
 	})
+	for _, route := range routes {
+		fmt.Printf("  %-6s %-25s\n", route.Method, route.Path)
+	}
+}
+
+func setupEngine(registry *prometheus.Registry) *gin.Engine {
+	gin.SetMode(gin.ReleaseMode)
 	engine := gin.New()
 
 	engine.Use(gin.LoggerWithFormatter(func(param gin.LogFormatterParams) string {
@@ -61,8 +73,20 @@ func main() {
 		)
 	}))
 	engine.Use(gin.Recovery())
+
+	metricsCfg := metrics.Config{
+		Registry: registry,
+	}
+	prometheusMiddleware := middleware.New(middleware.Config{
+		Recorder: metrics.NewRecorder(metricsCfg),
+		Service:  fmt.Sprintf("btfhub-online-%s", uuid.NewString()),
+	})
 	engine.Use(ginmiddleware.Handler("", prometheusMiddleware))
 
+	return engine
+}
+
+func addRoutes(engine *gin.Engine, routesHandler handlers.RoutesHandler) {
 	apiRouterGroup := engine.Group("/api")
 	v1RouterGroup := apiRouterGroup.Group("/v1")
 	v1RouterGroup.POST("/customize", routesHandler.CustomizeBTF)
@@ -73,18 +97,38 @@ func main() {
 	engine.POST("/generate", routesHandler.CustomizeBTFLegacy)
 	engine.GET("/list", routesHandler.ListBTFsLegacy)
 
-	// Serve our metrics.
-	if args.MetricsPort != "" {
-		go func() {
-			metricsAddress := fmt.Sprintf("0.0.0.0:%s", args.MetricsPort)
-			log.Printf("Metrics listening at %s", metricsAddress)
-			if err := http.ListenAndServe(metricsAddress, promhttp.Handler()); err != nil {
-				log.Panicf("Error while serving metrics: %s", err)
-			}
-		}()
+	monitoringRouterGroup := engine.Group("/monitoring")
+	if !args.DisableMonitoring {
+		monitoringRouterGroup.GET("/metrics", gin.WrapH(promhttp.Handler()))
+	}
+	healthHandler := healthcheck.NewHandler()
+	monitoringRouterGroup.GET("/health", gin.WrapF(healthHandler.LiveEndpoint))
+}
+
+func main() {
+	arg.MustParse(&args)
+
+	toolsDir, err := path.GetAbsolutePath(args.ToolsDir)
+	if err != nil {
+		log.Fatalf("Failed to find %s due to: %+v", args.ToolsDir, err)
 	}
 
-	fmt.Printf("listening on 0.0.0.0:%s\n", args.Port)
+	archive, err := btfarchive.NewGCSBucketArchive(args.BucketName)
+	if err != nil {
+		log.Fatalf("Failed initialize GCP bucket %q archive due to: %+v", args.BucketName, err)
+	}
+
+	// Create our middleware.
+	registry := prometheus.NewRegistry()
+	engine := setupEngine(registry)
+	addRoutes(engine, handlers.NewRoutesHandler(archive, toolsDir))
+	printAllRoutes(engine)
+
+	if args.PrometheusPushGateway != "" {
+		go pushPrometheusMetrics(args.PrometheusPushGateway, registry)
+	}
+
+	log.Printf("listening on 0.0.0.0:%s\n", args.Port)
 	if err := engine.Run(fmt.Sprintf("0.0.0.0:%s", args.Port)); err != nil {
 		log.Fatal(err)
 	}
--- go.mod
+++ go.mod
@@ -8,6 +8,8 @@ require (
 	github.com/alexflint/go-arg v1.4.2
 	github.com/gin-gonic/gin v1.7.7
 	github.com/go-playground/validator/v10 v10.10.0 // indirect
+	github.com/google/uuid v1.3.0
+	github.com/heptiolabs/healthcheck v0.0.0-20211123025425-613501dd5deb
 	github.com/prometheus/client_golang v1.12.0
 	github.com/rotisserie/eris v0.5.1
 	github.com/slok/go-http-metrics v0.10.0
@@ -15,4 +17,5 @@ require (
 	golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce // indirect
 	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2
 	google.golang.org/api v0.65.0
+	gopkg.in/DATA-DOG/go-sqlmock.v1 v1.3.0 // indirect
 )
